752b0b0e397e9c4b2805c5e5de21a440e3a60f2f
antisleepman <antisleepmantv@gmail.com>
2022-10-08 00:47:34 +0400

    end

--- src/App.js
+++ src/App.js
@@ -1,173 +1,181 @@
 import { useEffect, useState } from "react";
 import "./scss/index.scss";
-
 function App() {
-  let [price, setPrice] = useState(3_300_000);
+  const [price, setPrice] = useState(3_300_000);
+  const [off, setOff] = useState(false);
   const [term, setTerm] = useState(13);
   const [months, setmonths] = useState(60);
   const [anInitialFee, setAnInitialFee] = useState(420_000);
   const [AmountLeaseAgreement, SetAmountLeaseAgreement] = useState(4_467_313);
   const [monthslyPayment, SetmonthslyPayment] = useState(114_455);
-  function setData(term, price, months) {
-    setAnInitialFee((term / 100) * price);
-    SetAmountLeaseAgreement(anInitialFee + months * monthslyPayment);
-    SetmonthslyPayment(
-      (price - anInitialFee) *
-        ((0.035 * Math.pow((1 + 0.035), months)) /
-          (Math.pow((1 + 0.035), months) - 1))
-        )
+  for (let e of document.querySelectorAll(
+    'input[type="range"].slider-progress'
+  )) {
+    e.style.setProperty("--value", e.value);
+    e.style.setProperty("--min", e.min === "" ? "0" : e.min);
+    e.style.setProperty("--max", e.max === "" ? "100" : e.max);
+    e.addEventListener("input", () => e.style.setProperty("--value", e.value));
   }
-  for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
-    e.style.setProperty('--value', e.value);
-    e.style.setProperty('--min', e.min === '' ? '0' : e.min);
-    e.style.setProperty('--max', e.max === '' ? '100' : e.max);
-    e.addEventListener('input', () => e.style.setProperty('--value', e.value));
-  }
-const sendData  =  async () => {
-  let data = {
-    price: price,
-    term: term,
-    months: months,
-    initial: anInitialFee,
-    AmountLeaseAgreement: AmountLeaseAgreement,
-    monthslyPayment: monthslyPayment,
-  }
-  const response = await fetch('https://eoj3r7f3r4ef6v4.m.pipedream.net', {
-    method:'POST',
-    headers: { "Content-Type": "application/json"},
-    body: JSON.stringify(data),
-  })
+  const onSubmit = async () => {
+    const data = {
+      price: price,
+      term: term,
+      months: months,
+      anInitialFee: anInitialFee,
+      AmountLeaseAgreement: AmountLeaseAgreement,
+      monthslyPayment: monthslyPayment,
+    };
 
-  if(!response.ok) throw new Error(`ERROR ${response}`)
-  return await response.json()
-}
+    try {
+      const response = await fetch("https://eoj3r7f3r4ef6v4.m.pipedream.net", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      return await (response.json(), setOff(false));
+    } catch (e) {
+      throw new Error(`ERROR`);
+    }
+  };
 
-  /* подписка на изменения price, term, months */
   useEffect(() => {
     /* сброс к ближайшему корректному числу */
-    if (months > 60 || months < 1) {
-      if (months < 31) {
-        setmonths(1);
-        setData(term, price, months);
-      } else {
-        setmonths(60);
-        setData(term, price, months);
-      }
-    }
-    if (price > 6_000_000 || price < 1_000_000) {
-      if (price < 3_000_001) {
-        setPrice(1_000_000);
-        setData(term, price, months);
-      } else {
-        setPrice(6_000_000);
-        setData(term, price, months);
-      }
-    }
-    if (term > 60 || term < 10) {
-      if (term < 31) {
-        setTerm(10);
-        setData(term, price, months);
-      } else {
-        setTerm(60);
-        setData(term, price, months);
-      }
-    }
-    setData(term, price, months);
-  }, [term, price, months]);
+  if (price > 6000000 || price < 1000000) {
+    if (price < 3000001) setPrice(1000000);
+    else setPrice(6000000);
+  }
+  if (months > 60 || months < 1) {
+    if (months < 31) setmonths(1);
+    else setmonths(60);
+  }
+  if (term > 60 || term < 10) {
+    if (term < 31) setTerm(10);
+    else setTerm(60);
+  }
+    setTimeout(() => {
+      setAnInitialFee(Math.round((term / 100) * price));
+      SetAmountLeaseAgreement(
+        Math.round(anInitialFee + months * monthslyPayment)
+      );
+      SetmonthslyPayment(
+        Math.round(
+          (price - anInitialFee) *
+            ((0.035 * Math.pow(1 + 0.035, months)) /
+              (Math.pow(1 + 0.035, months) - 1))
+        )
+      );
+    }, 10);
+  }, [months, price, term, anInitialFee, monthslyPayment]);
   return (
-    <form className="container">
+    <div className="container">
       <h1 className="headtext">Рассчитайте стоимость автомобиля в лизинг</h1>
       <div className="inputs">
-        <div className="inputForm">
+        <div className="inputForm" disabled={off}>
           <h3 className="mainLabel">Стоимость автомобиля</h3>
           <div className="Shapes">
-            <h2 className="numberinput">
+            <div className="line">
               <input
                 type="tel"
+                value={new Intl.NumberFormat("ru").format(price)}
                 inputMode="numeric"
                 className="numberinput"
-                min={1_000_000}
-                max={6_000_000}
-                onChange={(e) => setPrice(e.target.value)}
-                value={price}
+                onChange={(e) => setPrice(e.target.value.replace(/\s/g, ""))}
               />
-              ₽
-            </h2>
+              <h2 className="number">₽</h2>
+            </div>
             <input
               type="range"
               min={1_000_000}
               max={6_000_000}
               value={price}
               className="progress slider-progress"
-              onChange={(e) => setPrice(e.target.value)}
+              onChange={(e) => setPrice(e.target.value.replace(/\s/g, ""))}
             />
           </div>
         </div>
-        <div className="inputForm">
+        <div className="inputForm" disabled={off}>
           <h3 className="mainLabel">Первоначальный взнос</h3>
           <div className="Shapes">
-            <h2 className="numberinput">
-              {Math.round(anInitialFee)}₽
-             
-              <input
-                className="percent"
-                type="number"
-                max={60}
-                min={10}
-                value={term}
-                onChange={(e) => setTerm(e.target.value)}
-              />
-               <label className="mouth">%
-              </label>
-            </h2>
+            <div className="line">
+              <h2 className="numberinput">
+                {new Intl.NumberFormat("ru").format(Math.round(anInitialFee))} ₽
+              </h2>
+              <div className="percent-box">
+                <input
+                  className="percent"
+                  type="number"
+                  max={60}
+                  min={10}
+                  value={term}
+                  onChange={(e) => setTerm(e.target.value.replace(/\s/g, ""))}
+                />
+                <h2>%</h2>
+              </div>
+            </div>
             <input
               type="range"
               max={60}
               min={10}
               step={1}
               value={term}
-              onChange={(e) => setTerm(e.target.value)}
+              onChange={(e) => setTerm(e.target.value.replace(/\s/g, ""))}
               className="progress slider-progress"
-            ></input>
+            />
           </div>
         </div>
-        <div className="inputForm">
+        <div className="inputForm" disabled={off}>
           <h3 className="mainLabel">Срок лизинга</h3>
           <div className="Shapes">
-            <h2 className="numberinput">
+            <div className="line">
               <input
                 type="number"
                 min={1}
                 max={60}
-                className='numberinput'
+                className="numberinput"
                 value={months}
-                onChange={(e) => setmonths(e.target.value)}
+                onChange={(e) => setmonths(e.target.value.replace(/\s/g, ""))}
               />
-              <label className="mouth">мес.</label>
-            </h2>
+              <h2 className="number">мес.</h2>
+            </div>
             <input
               type="range"
               min={1}
               max={60}
               value={months}
-              onChange={(e) => setmonths(e.target.value)}
+              onChange={(e) => setmonths(e.target.value.replace(/\s/g, ""))}
               className="progress slider-progress"
             ></input>
           </div>
         </div>
       </div>
-      <div className="results" >
-      <div className="result">
-        <h3 className="title">Сумма договора лизинга</h3>
-        <h1 className="content">{Math.round(AmountLeaseAgreement)}₽</h1>
+      <div className="results">
+        <div className="result">
+          <h3 className="title">Сумма договора лизинга</h3>
+          <h1 className="content">
+            {new Intl.NumberFormat("ru").format(
+              Math.round(AmountLeaseAgreement)
+            )}{" "}
+            ₽
+          </h1>
+        </div>
+        <div className="result">
+          <h3 className="title">Ежемесячный платеж от</h3>
+          <h1 className="content">
+            {new Intl.NumberFormat("ru").format(Math.round(monthslyPayment))} ₽
+          </h1>
+        </div>
+        <button
+          className="buttomsend"
+          disabled={off}
+          onClick={() => {
+            onSubmit();
+            setOff(true);
+          }}
+        >
+          {off?<div class="lds-dual-ring"/>:`Оставить заявку`}
+        </button>
       </div>
-      <div className="result">
-        <h3 className="title">Ежемесячный платеж от</h3>
-        <h1 className="content">{Math.round(monthslyPayment)}₽</h1>
-      </div>
-      <button onClick={sendData} className="buttomsend" >Оставить заявку</button>
-      </div>
-    </form>
+    </div>
   );
 }
 export default App;
